c9a9f0500415a394adf375431852a1728ad8bb8c
Ed Carrel <edward@carrel.org>
2013-12-11 00:26:31 -0800

    Removing dead code.

--- test/unit/test_helper.rb
+++ test/unit/test_helper.rb
@@ -5,7 +5,6 @@ Coveralls.wear!
 
 if ENV["ENABLE_SIMPLE_COV"]
   require 'simplecov'
-#  require File.expand_path('../simplecov_helper', __FILE__)
   SimpleCov.start do
     add_filter "test"
     add_group 'API',          "lib"
